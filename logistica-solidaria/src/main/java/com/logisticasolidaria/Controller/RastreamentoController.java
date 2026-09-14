6c9d7e969659cc1dfacca03e8c72d6956ff46ef9
pedromorais4709-arch <pedro.morais4709@gmail.com>
2026-09-14 19:52:49 -0300

    Update RastreamentoController.java
    Corrigr rota duplicada

--- logistica-solidaria/src/main/java/com/logisticasolidaria/Controller/RastreamentoController.java
+++ logistica-solidaria/src/main/java/com/logisticasolidaria/Controller/RastreamentoController.java
@@ -2,7 +2,6 @@ package com.logisticasolidaria.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/rastreamento")
